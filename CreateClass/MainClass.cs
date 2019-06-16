530a4438fadf15d03f3f6d0a8ac596016d810f0e
AndreaSzoboszlai <szobandi@gmail.com>
2019-06-16 19:22:32 +0200

    1st task

---
+++ CreateClass/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Andi", "1991.06.16.", CreateClass.MainClass.Genders.female);
+
+            Console.WriteLine(person1);
+
+            Console.ReadLine();
+        }
+
+        public enum Genders
+        {
+            male = 0,
+            female = 1
+        }
+    }
+}
---
+++ CreateClass/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        private string name;
+        private string birthday;
+        private CreateClass.MainClass.Genders gender;
+
+
+        public Person(string name, string birthday, CreateClass.MainClass.Genders gender)
+        {
+            this.name = name;
+            this.birthday = birthday;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + name + " " + birthday + " " + gender;
+        }
+
+    }
+}
